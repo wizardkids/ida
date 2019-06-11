dccf1bf8e9921aa0fe99985871e2dc0ca46743d8
Rick Rawson <rick.rawson@outlook.com>
2019-06-11 13:31:15 -0400

    dev 2019-06-11 01:31 PM

--- RSS_reader.py
+++ RSS_reader.py
@@ -5,7 +5,7 @@ Richard E. Rawson
 2019-06-09
 
 Program Description:
-    A small, lightweight, RSS feed reader. 
+    A small, lightweight, RSS feed reader.
 
 Features:
 -- Add, edit, delete URLs
@@ -14,43 +14,41 @@ Features:
 -- keep track of how frequently the feed is updated
 -- keep track of last time the feed was updated
 -- notify if a feed is unreachable
--- display each RSS site and a list of its titles
+√-- display each RSS site and a list of its titles
 -- order the titles from newest to oldest
--- load a title in a browser
+√-- load a title in a browser
 -- provide a mechanism for selecting a particular title to read
 -- mark a read title as "read"
 -- manually mark a feed title as read or unread
 -- automatically update all feeds when app starts update
 -- background mode updates feed in the background
--- group feeds so they are easier to find
--- import/export
--- list feeds by group/title
+√-- group feeds so they are easier to find
+√-- import OPML
+-- export
+√-- list feeds by group/title
 -- save a feed to OneNote
 
 """
 
 
+import os
 import re
+import textwrap
+import webbrowser
+from inspect import getmembers, isfunction, getfullargspec
 from pprint import pprint
+from sys import modules
 
 import feedparser
 import requests
 import urlwatch
-from sys import modules
-import webbrowser
-import os
-import textwrap
-from inspect import getmembers, isfunction
+import json
 
 
 def print_all_functions():
     """
-    All commands and math operations. 
-
-    This function is not used, except by the developer.
+    Print all the functions, with their docStrings, used in this program. This function is not used, except by the developer.
     """
-
-    # strategy: only get docstrings from things NOT in this list; this will be all the fxns that the user can use
     module_functions = []
 
     func_name = ''
@@ -60,59 +58,107 @@ def print_all_functions():
 
     for i in getmembers(modules[__name__],
                         predicate=lambda f: isfunction(f) and f.__module__ == __name__):
-        print(i[0])
-        # func = i[0]
-        # fnd = False
-        # if func in module_functions:
-        #     continue
-        # for k, v in op2.items():
-        #     if v[0].__name__ == func:
-        #         fnd = True
-        #         func_name = k
-        #         docString = v[1]
-        #         break
-        # if not fnd:
-        #     continue
+        # i[0] is the function name
+        # i[1] is the function itself
 
-        # print('{:>8}'.format(func_name), ' | ', sep='', end='')
-        # wrapper = textwrap.TextWrapper(width=line_width)
-        # explanation = wrapper.wrap(text=docString)
-        # for element in explanation:
-        #     print(element)
+        # get the arguments for the i[1]
+        v_args = getfullargspec(i[1])
+        this_args = ''
+        if v_args[0]:
+            for j in range(len(v_args[0])):
+                if this_args:
+                    this_args = this_args + ', ' + v_args[0][j]
+                else:
+                    this_args = v_args[0][j]
+
+        print(i[0], '(', this_args, ')', sep='', end='')
+        print(' '*5, i[1].__doc__, sep='')
+
     return
 
 
-def get_rss_status(url):
+def retrieve_myFeeds():
     """
-    Get the content from a URL.
+    Read myFeeds.json and return a dictionary of the RSS feeds. Each key is a group and each value is a list of RSS feeds in that group. Each feed in 'value' contains two elements: title and RSS address.
+    """
+    try:
+        with open("myFeeds.json", 'r') as file:
+            myFeeds = json.load(file)
+    except FileNotFoundError:
+        myFeeds = {}
+
+    # for k, v in myFeeds.items():
+        # print('\n', k, sep='')
+        # for ndx, i in enumerate(v):
+        # print(' '*5, ndx+1, ': ', i[0], sep='')
+        # print(' '*5, ndx+1, ': ', i[0], '\n', ' '*10, i[1], sep='')
+        # x = input('PRESS <ENTER> TO CONTINUE...')
+
+    return myFeeds
+
+
+def inquire_feed(rss):
+    """
+    Check a website to see if it has changed.
+    """
+    # -- https://pythonhosted.org/feedparser/http-etag.html
+
+    # first request
+    feed = feedparser.parse(rss)
+
+    # store the etag and modified; either or both may not exist!
+    try:
+        last_etag = feed.etag
+    except AttributeError:
+        last_etag = ''
+    try:
+        last_modified = feed.modified
+    except AttributeError:
+        last_modified = ''
+
+    # check if new version exists by sending etag and modified back to the server
+    if last_etag and last_modified:
+        feed_update = feedparser.parse(
+            rss, etag=last_etag, modified=last_modified)
+    elif last_etag and not last_modified:
+        feed_update = feedparser.parse(rss, etag=last_etag)
+    elif last_modified and not last_etag:
+        feed_update = feedparser.parse(rss, modified=last_modified)
+    else:
+        feed_update = feedparser.parse(rss)
+
+    return feed_update.status
+
+
+def get_url_status(url):
+    """
+    Get the status code for a URL.
     """
     # set the headers like we are a browser
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     # download the url
     r = requests.get(url, headers=headers)
-    # print(r.status_code, r.headers['content-type'], r.encoding)
-    # print()
-    # txt = r.text
+
     return r.status_code
 
 
-def show_HTML_string(HTML_string):
-    """
-    Given a string, convert the string into an HTML file (temp.html) and display in a browser window.
-    """
-    path = os.path.abspath('temp.html')
-    url = 'file://' + path
+# ? def show_HTML_string(HTML_string):
+# ?     """
+# ?     Given a string, convert the string into an HTML file (temp.html) and  ?display in a browser window.
+# ?     """
+# ?     path = os.path.abspath('temp.html')
+# ?     url = 'file://' + path
 
-    with open(path, 'w') as f:
-        f.write(HTML_string)
-    webbrowser.open(url)
-    return None
+# ?     with open(path, 'w') as f:
+# ?         f.write(HTML_string)
+# ?     webbrowser.open(url)
+# ?     return None
 
 
 def get_latest_feed(rss):
     """
-    Get a news feed.
+    Utility function to print information about a news feed. Used only by the developer.
     """
     try:
         newsfeed = feedparser.parse(rss)
@@ -124,31 +170,42 @@ def get_latest_feed(rss):
                         'author_detail', 'tags', 'id', 'guidislink', 'summary', 'summary_detail', 'content', 'wfw_commentrss', 'slash_comments', 'media_content']
 
         # print site information
-        # print('\n\n')
-        # print('\nSite title:', newsfeed['feed']['title'])
-        # print('\nSite link:', newsfeed['feed']['link'])
-        # print('\nSite subtitle:', newsfeed['feed']['subtitle'])
-        # print('\nSite updated:', newsfeed['feed']['updated'])
+        print('\nSite title:', newsfeed['feed']['title'])
+        print('\nSite link:', newsfeed['feed']['link'])
+        print('\nSite subtitle:', newsfeed['feed']['subtitle'])
+        print('\nSite updated:', newsfeed['feed']['updated'])
 
         # print page information
-        # print('\n\n')
-        # print('\nPage title:', newsfeed['entries'][0]['title'])
-        # print('\nPage id:', newsfeed['entries'][0]['id'])
-        # print('\nPage published:', newsfeed['entries'][0]['published'])
+        print('\nPage title:', newsfeed['entries'][0]['title'])
+        print('\nPage id:', newsfeed['entries'][0]['id'])
+        print('\nPage published:', newsfeed['entries'][0]['published'])
 
         # get the content for first item in [entries]
-        c = '<html><body>' + \
-            '<H1>' + newsfeed['entries'][0]['title'] + '</H1>' + \
-            newsfeed['entries'][0]['content'][0]['value'] + '</body></html>'
+        c = newsfeed['entries'][0]['content'][0]['value']
+        # print(c)
     except IndexError:
-        return rss
+        pass
 
-    return newsfeed['entries'][0]['link']
+    return
+
+
+def last_ten(rss):
+    """
+    Return the titles of the latest ten pages for a website as a list of titles.
+    """
+    newsfeed = feedparser.parse(rss)
+    num_entries = len(newsfeed['entries'])
+
+    last_ten_titles = []
+    for i in range(num_entries):
+        last_ten_titles.append(newsfeed['entries'][i]['title'])
+
+    return last_ten_titles
 
 
 def show_lastest_rss(rss):
     """
-    Show the most recent feed in a browser window.
+    Show an RSS website in a browser window.
     """
     try:
         webbrowser.open(rss)
@@ -159,7 +216,9 @@ def show_lastest_rss(rss):
 
 def import_OPML():
     """
-    Parse an OPML file and put RSS addresses in [myFeeds]. If no file is entered, process is aborted. If FileNotFoundError is generated, notify user and <continue>. [myFeeds] is written to myFeeds.txt. This file is overwritten each time an OPML file is read.
+    Parse an OPML file and put group names, RSS titles, and RSS addresses in {myFeeds} in the form {'Group name': [[title1, RSS1], [title2, RSS2]]}. If no filename is entered, process is aborted. If FileNotFoundError is generated, notify user and <continue>. With confirmation, this file is overwritten each time an OPML file is read.
+
+    Output: {myFeeds} is written to myFeeds.json.
     """
     while True:
         # file = input('Name of OPML file to import: ')
@@ -172,38 +231,73 @@ def import_OPML():
                 print('\n', file, ' not found.\n')
                 continue
 
+            # extract group name from line
+            rem_Feed_Group = re.compile(r"""
+                        (.*<outline\stext=\")(?P<Feed_Group>\w*\s*\w*)
+                        """, re.X)
+            # extract site title from the line
+            rem_title = re.compile(r"""
+                        (.*)(<outline.*text=\")(?P<Feed_Title>.*)(\"\stitle)
+                        """, re.X)
+
             # extract the RSS address from the line
-            rem = re.compile(r"""
+            rem_RSS = re.compile(r"""
                             (.xmlUrl=\")(?P<RSS>.*)(\"\s)
                             """, re.X)
 
-            myFeeds = []
+            # extract HTML address from the line
+            rem_HTML = re.compile(r"""
+                            (.*htmlUrl=")(?P<URL>.*\")
+                            """, re.X)
+
+            # put Group, title, and RSS in {myFeeds}
+            myFeeds = {}
             for ndx, line in enumerate(feedly):
-                m = re.search(rem, line)
-                if m:
-                    # print(m.group('RSS'))
-                    myFeeds.append(m.group('RSS'))
+                m_Feed_Group = re.search(rem_Feed_Group, line)
+                m_Feed_Title = re.search(rem_title, line)
+                m_RSS = re.search(rem_RSS, line)
+                m_HTML = re.search(rem_HTML, line)
+                if m_Feed_Group:
+                    this_group = m_Feed_Group.group("Feed_Group")
+                    myFeeds.update({this_group: []})
+                if m_Feed_Title:
+                    this_title = m_Feed_Title.group("Feed_Title")
+                if m_RSS:
+                    this_RSS = m_RSS.group('RSS')
+                if m_HTML:
+                    this_URL = m_HTML.group('URL')[:-1]  # deletes the final "
+                if m_Feed_Title and m_RSS and m_HTML:
+                    marked = False
+                    new_feed = (this_title, this_RSS, this_URL, marked)
+                    current_feed = myFeeds[this_group]
+                    current_feed.append(new_feed)
+                    myFeeds.update({this_group: current_feed})
             break
         else:
             print('Aborted.')
             return
 
-    # with confirmation, write myFeeds to a file of the same name
+    # for k, v in myFeeds.items():
+    #     print('\n', k, sep='')
+    #     for ndx, i in enumerate(v):
+    #         print(' '*5, ndx+1, ': ', i[0], sep='')
+
+    # with confirmation, write {myFeeds} to myFeeds.json
     r = input('Overwrite "myFeeds.txt"? (YES/ABORT) ')
     if r.upper() == 'YES':
-        print('Writing to myFeeds.txt...')
-        with open('myFeeds.txt', 'w') as f:
-            for line in myFeeds:
-                f.write(line + '\n')
+        with open('myFeeds.json', 'w+') as file:
+            file.write(json.dumps(myFeeds, ensure_ascii=False))
+        err = False
     else:
         print('Aborted.')
+        err = True
 
-    return
+    return err
 
 
 def fold(txt):
     """
-    Textwraps 'txt' 45 characters wide.
+    Utility function that textwraps 'txt' 45 characters wide.
     """
     return textwrap.fill(txt, width=45)
 
@@ -239,26 +333,84 @@ def get_revision_number():
 
     print("\nREVISION NUMBER:", revision_delta.days)
     print('This is the number of days since ', start_date, '\n',
-          'the date that the first version of this\n', 'calculator was launched.\n\n', sep='')
+          'the date that the first version of "ida" was launched.\n\n', sep='')
     return None
+
+
+def pick_website():
+    """
+    From a list of feeds, pick one and return the RSS address.
+    """
+    myFeeds = retrieve_myFeeds()
+
+    cnt, the_titles = 0, []
+    for k, v in myFeeds.items():
+        print('\n', k, sep='')
+        for ndx, i in enumerate(v):
+            print(' '*5, cnt+1, ': ', i[0], sep='')
+            the_titles.append(i[0])
+            cnt += 1
+
+    while True:
+        site_num = input('Which site? ')
+        if not site_num.strip():
+            break
+        try:
+            site_num = int(site_num)
+            if site_num > 0 and site_num <= cnt:
+                break
+            else:
+                print("Enter a number between 1 and ", cnt, sep='')
+                continue
+        except:
+            print('Enter only an integer.')
+            continue
+
+    site_name = the_titles[site_num-1]
+
+    # find the site name in {myFeeds}
+    rss = ''
+    for group in myFeeds.values():
+        for site in group:
+            if site[0] == site_name:
+                rss = site[2]
+                return rss
+
+    return rss
 
 
 if __name__ == '__main__':
 
     # get_revision_number()
-    version_num = '0.1 rev0'
+    version_num = '0.1 rev1'
     print('ida ' + version_num[0:3] + ' - a small news feed reader')
 
     print_all_functions()
-    # about this project
+
+    # -- about this project
     # about()
 
-    # import OPML file and store RSS addresses in myFeeds.txt
-    # import_OPML()
+    # -- print various attributes of a single RSS feed
+    # get_latest_feed('https://hebendsdown.wordpress.com/feed/')
 
-    # rss = get_latest_feed('https://hebendsdown.wordpress.com/feed/')
+    # -- import OPML file and store RSS info in myFeeds.json; return True if successful
+    # err = import_OPML()
 
-    # if get_rss_status(rss) == 200:
-    #     show_lastest_rss(rss)
+    # -- retrieve all feeds from myFeeds.json and return {myFeeds} as a dict
+    # myFeeds = retrieve_myFeeds()
+
+    # -- get last ten RSS feeds from a single site, returned as a list
+    # last_ten_titles = last_ten('https://hebendsdown.wordpress.com/feed/')
+
+    # -- check to see if feed has changed; return status code
+    #  -- status is 304 if no change
+    # status = inquire_feed('https://hebendsdown.wordpress.com/feed/')
+
+    # -- pick one website from the list and return the URL (not RSS)
+    # url = pick_website()
+
+    # # -- display an RSS address, if it exists, in a browser window
+    # if get_url_status(url) == 200:
+    #     show_lastest_rss(url)
     # else:
-    #     print('Sorry. We could not find ', rss, sep='')
+    #     print('Sorry. We could not find ', url, sep='')
